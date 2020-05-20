f5a2b03f05eff6af4758b5825cf882dbe6f4e54e
Hongtaek Ju <hongtaek.ju@gmail.com>
2020-05-20 15:15:20 +0900

    add comment to accept

--- acceptance_test.bash
+++ acceptance_test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This is a test program for calculator
 
 test $(curl --silent -X GET http://docker:8090/sum?a=1\&b=2) -eq 3 && \
 test $(curl --silent -X GET http://docker:8090/sum?a=1000000\&b=2000000) -eq 3000000 && \
