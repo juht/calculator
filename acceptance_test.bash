1a2fd900127c0910ad6717a9597cc6dc651c156d
ABC <xyz@abc.com>
2020-06-07 23:09:02 +0900

    zz

--- acceptance_test.bash
+++ acceptance_test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-test $(curl --silent -X GET http://docker:8090/sum?a=1\&b=2) -eq 4 && \
+test $(curl --silent -X GET http://docker:8090/sum?a=1\&b=2) -eq 3 && \
 test $(curl --silent -X GET http://docker:8090/sum?a=2\&b=\-2) -eq 0 && \
 test $(curl --silent -X GET http://docker:8090/sum?a=1000000\&b=2000000) -eq 3000000 && \
 test $(curl --silent -X GET http://docker:8090/sum?a=\-1\&b=\-2) -eq \-3 && \
